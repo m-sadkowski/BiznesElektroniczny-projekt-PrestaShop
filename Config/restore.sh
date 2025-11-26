3b3919c373a0ddef6cbec352e7a01e4a731a4c4b
Ostap <ostaplozovyy25@gmail.com>
2025-11-26 01:08:31 +0100

    added script to restore data from dump file

---
+++ Config/restore.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# --- KONFIGURACJA ---
+DB_SERVICE="db"
+DB_USER="prestashop_user"
+DB_PASSWD="user123"
+DB_NAME="prestashop"
+PS_SERVICE="prestashop"
+
+# Ścieżka do pliku zrzutu
+DUMP_FILE="./export/prestashop_dump.sql"
+
+# Sprawdzenie czy plik dumpa istnieje
+if [ ! -f "$DUMP_FILE" ]; then
+    echo "BŁĄD: Nie znaleziono pliku $DUMP_FILE"
+    exit 1
+fi
+
+# Wgrywanie zrzutu SQL
+echo "Importowanie danych z $DUMP_FILE..."
+cat "$DUMP_FILE" | docker compose exec -T $DB_SERVICE mysql -u$DB_USER -p$DB_PASSWD $DB_NAME
+
+if [ $? -eq 0 ]; then
+    echo "Sukces: Baza danych została przywrócona."
+else
+    echo "BŁĄD: Wystąpił problem przy imporcie SQL."
+    exit 1
+fi
+
+# 5. Czyszczenie cache PrestaShop
+docker compose exec -T $PS_SERVICE rm -rf /var/www/html/var/cache/prod /var/www/html/var/cache/dev &>/dev/null
+
+echo "PRZYWRACANIE ZAKOŃCZONE POMYŚLNIE."
