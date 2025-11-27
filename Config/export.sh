1536d329eb411d93eb73058577afebc498351360
Ostap <ostaplozovyy25@gmail.com>
2025-11-27 16:03:08 +0100

    product modifications added

--- Config/export.sh
+++ Config/export.sh
@@ -5,27 +5,10 @@ DB_USER="prestashop_user"
 DB_PASSWD="user123"
 DB_NAME="prestashop"
 DB_SERVICE="db" 
-DB_PREFIX="ps_"
 
 EXPORT_DIR="./export"
 FILENAME="prestashop_dump.sql"
 FULL_PATH="${EXPORT_DIR}/${FILENAME}"
-
-IGNORE_TABLES_SUFFIXES=(
-    "connections"
-    "connections_source"
-    "guest"
-    "log"
-    "statssearch"
-    "pagenotfound"
-    "date_range"
-)
-
-IGNORE_STRING=""
-for SUFFIX in "${IGNORE_TABLES_SUFFIXES[@]}"; do
-    TABLE="${DB_PREFIX}${SUFFIX}"
-    IGNORE_STRING+=" --ignore-table=${DB_NAME}.${TABLE}"
-done
 
 # Sprawdzenie i utworzenie folderu docelowego
 if [ ! -d "$EXPORT_DIR" ]; then
@@ -39,7 +22,6 @@ echo "Rozpoczynam eksport do pliku $FULL_PATH..."
 docker compose exec -T $DB_SERVICE mysqldump \
   -u $DB_USER \
   -p$DB_PASSWD \
-  $IGNORE_STRING \
   $DB_NAME \
   > "$FULL_PATH"
 
--- Source/README.md
+++ Source/README.md
@@ -11,6 +11,3 @@ Ten folder przechowuje całą dodatkową logikę i funkcjonalność sklepu.
 
 ### 2. `themes/`
 Ten folder zawiera wszystkie szablony, style i pliki graficzne, które określają ostateczny wygląd i styl  sklepu.
-
-### 3. `img/`
-Ten folder zawiera zdjęcia produktów zarejestrowanych w sklepie, zdjęcia powinny być kompatybilne z plikiem eksportu sklepu `prestashop_dump.sql` znajdującego się w folderze `Config/export`
